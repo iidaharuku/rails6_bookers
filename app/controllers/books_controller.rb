a8ea4421f23bf19c59391c11c528b1d88fc1978d
iidaharuku <hi276676@gmail.com>
2022-09-13 18:11:57 +0900

    Add book_create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,21 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def
+    params.require(:book).permit(:title, :body)
   end
 end
